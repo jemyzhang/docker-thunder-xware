a64aadbe9c47b82fe8412853bb8b878b2ee35906
jemyzhang <jemy.zhang@gmail.com>
2015-05-15 11:26:20 +0800

    restart the container if the named container is exists

--- run.sh
+++ run.sh
@@ -1,14 +1,24 @@
+# check the existence of data folder
 if [ ! -d data ]; then
     mkdir -p data
 fi
 
+# check the existence of the xware container
+# build the container if is not exists
 docker images | grep docker-xware
 if [ ! $? -eq 0 ]; then
     docker build -t docker-xware .
 fi
 
+# start a named container: xware
 docker run -d --privileged=true \
         --name=xware \
         -v $(pwd)/data:/app/TDDOWNLOAD \
         -v $(pwd)/thunder/bin:/app/bin \
     docker-xware
+
+# if docker is already running, or the host system was restart,
+# then restart the container
+if [ ! $? -eq 0 ]; then
+    docker restart xware
+fi
